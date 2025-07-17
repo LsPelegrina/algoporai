90075ae06df7b3b62e0b87ed47e8f0677c2532c1
LsPelegrina <lspelegrina@gmail.com>
2025-07-17 19:49:59 -0300

    vamoo

--- src/main/java/com/rinha/core/model/PaymentSummary.java
+++ src/main/java/com/rinha/core/model/PaymentSummary.java
@@ -1,10 +1,13 @@
 package com.rinha.core.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 import java.util.Map;
 
 @RegisterForReflection
 public record PaymentSummary(
+        @JsonProperty("default")
         Map<String, Object> defaultProcessor,
+        @JsonProperty("fallback")
         Map<String, Object> fallbackProcessor
 ) {}
